71b96894a8e17c5508bd2f357977db8bf5c04eb1
Lape15 <larape1993@gmail.com>
2020-07-07 11:45:55 +0100

    connected registration page to api

--- src/Pages/Auth/Register.jsx
+++ src/Pages/Auth/Register.jsx
@@ -1,19 +1,97 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
 import logo from "../../assets/budget-it.png";
 import { currency } from "./currency";
 import { TimelineLite } from "gsap";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
-const Register = () => {
-  const [user, setUser] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    currency: "NGN",
+const Register = (props) => {
+  const [authenticationError, setAuthenticationError] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  if (user) {
+    props.history.push("/dashboard");
+    localStorage.setItem("user", JSON.stringify(user.token));
+  }
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      currency: "NGN",
+    },
+
+    validationSchema: Yup.object({
+      firstname: Yup.string()
+        .min(2, "First name must be greater than 2 characters")
+        .required("Required")
+        .matches(
+          /[A-Za-z0-9 ]+$/,
+          "First name must  not contain special characters"
+        ),
+
+      lastname: Yup.string()
+        .min(2, "Last name must be greater than 2 characters")
+        .required("Required")
+        .matches(
+          /[A-Za-z0-9 ]+$/,
+          "Last name must not a contain special character"
+        ),
+
+      email: Yup.string()
+        .matches(
+          /^[a-z0-9][a-z0-9-_.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/,
+          "Email must be valid"
+        )
+        .required("Required"),
+
+      password: Yup.string()
+        .min(6, "Password is too short")
+        .required("Required"),
+
+      confirmPassword: Yup.string()
+        .min(6, "Password is too short")
+        .oneOf([Yup.ref("password")], "Passwords do not match"),
+
+      currency: Yup.string().required("Required or default set to NGN"),
+    }),
+
+    onSubmit: (values) => {
+      setLoading(true);
+      axios({
+        method: "post",
+        url: "https://atumaatu.herokuapp.com/v1/users",
+        data: JSON.stringify({
+          user: {
+            firstname: values.firstname,
+            lastname: values.lastname,
+            email: values.email,
+            password: values.password,
+            password_confirmation: values.confirmPassword,
+            currency: values.currency,
+          },
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then(function (response) {
+          setLoading(false);
+          setUser(response.data.payload);
+        })
+        .catch(function (error) {
+          if (error && error.response) {
+            setLoading(false);
+            setAuthenticationError(error.response.data.error);
+          }
+        });
+    },
   });
-  let auth = useRef(null);
+
   let tl = new TimelineLite({ paused: true });
 
   useEffect(() => {
@@ -68,19 +146,14 @@ const Register = () => {
     tl.play();
   }, []);
 
-  const handleChange = (e) => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleRegSubmit = (e) => {
-    e.preventDefault();
-  };
+  if (localStorage.user) {
+    return <Redirect to="/dashboard" />;
+  }
+
   return (
-    <div className="auth" ref={(el) => (auth = el)}>
+    <div className="auth">
       <div className="side">
-        <Link to="/">
+        <Link to="/" className="img-link">
           <img src={logo} alt="BudgetIt logo" />{" "}
         </Link>
 
@@ -93,25 +166,46 @@ const Register = () => {
         </div>
       </div>
       <div className="reg-form">
-        <form onSubmit={handleRegSubmit}>
+        <form onSubmit={formik.handleSubmit}>
+          {authenticationError ? <h3>{authenticationError}</h3> : null}
           <div className="w-80 first-wrapper">
             <div className="w-40">
               <label className="name">First name</label>
               <input
                 type="text"
-                value={user.firstname}
                 name="firstname"
-                onChange={handleChange}
+                {...formik.getFieldProps("firstname")}
+                className={`${
+                  formik.touched.firstname && formik.errors.firstname
+                    ? "border-red"
+                    : ""
+                }`}
               />
+              {formik.touched.firstname && formik.errors.firstname ? (
+                <span className="error">
+                  <i className="fas fa-times"></i>
+                  {formik.errors.firstname}
+                </span>
+              ) : null}
             </div>
             <div className="w-40">
               <label className="last-name">Last name</label>
               <input
                 type="text"
-                value={user.lastname}
-                onChange={handleChange}
                 name="lastname"
+                {...formik.getFieldProps("lastname")}
+                className={`${
+                  formik.touched.lastname && formik.errors.lastname
+                    ? "border-red"
+                    : ""
+                }`}
               />
+              {formik.touched.lastname && formik.errors.lastname ? (
+                <span className="error">
+                  <i className="fas fa-times"></i>
+                  {formik.errors.lastname}
+                </span>
+              ) : null}
             </div>
           </div>
           <div className="w-80">
@@ -119,40 +213,63 @@ const Register = () => {
             <em>So you can log into your account</em>
             <input
               type="email"
-              value={user.email}
-              onChange={handleChange}
               name="email"
+              {...formik.getFieldProps("email")}
+              className={`${
+                formik.touched.email && formik.errors.email ? "border-red" : ""
+              }`}
             />
+            {formik.touched.email && formik.errors.email ? (
+              <span className="error">
+                <i className="fas fa-times"></i>
+                {formik.errors.email}
+              </span>
+            ) : null}
           </div>
           <div className="w-80 pas">
             <label className="password">Password</label>
             <em>To keep your account secure</em>
             <input
               type="password"
-              value={user.password}
-              onChange={handleChange}
               name="password"
+              {...formik.getFieldProps("password")}
+              className={`${
+                formik.touched.password && formik.errors.password
+                  ? "border-red"
+                  : ""
+              }`}
             />
+            {formik.touched.password && formik.errors.password ? (
+              <span className="error">
+                <i className="fas fa-times"></i>
+                {formik.errors.password}
+              </span>
+            ) : null}
           </div>
           <div className="w-80 pas">
             <label className="conPassword">Confirm password</label>
             <em>To verify your password</em>
             <input
               type="password"
-              value={user.confirmPassword}
-              onChange={handleChange}
               name="confirmPassword"
+              {...formik.getFieldProps("confirmPassword")}
+              className={`${
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+                  ? "border-red"
+                  : ""
+              }`}
             />
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <span className="error">
+                <i className="fas fa-times"></i>
+                {formik.errors.confirmPassword}
+              </span>
+            ) : null}
           </div>
           <div className="w-80 sel">
             <label className="cur">Currency</label>
             <em>To verify your currency</em>
-            <select
-              value={user.currency}
-              name="currency"
-              onChange={handleChange}
-              onBlur={handleChange}
-            >
+            <select name="currency" {...formik.getFieldProps("currency")}>
               {currency.map((cur, index) => {
                 return (
                   <option value={cur.cc} key={index}>
@@ -163,7 +280,10 @@ const Register = () => {
             </select>
           </div>
           <div className="w-80">
-            <button>Create your free account</button>
+            <button type="submit" disabled={loading}>
+              Create your free account
+              {loading && <i class="fas fa-spinner"></i>}
+            </button>
           </div>
           <div className="w-80 back">
             {" "}
