a42eabb2ae6fbeb979a99c00aa943495aaf856b4
Akintan Lape <50051435+lape15@users.noreply.github.com>
2020-07-16 22:33:27 +0100

    edit/delete component (#11)
    * edit/delete component

--- src/Pages/Dashboard/CardBox.jsx
+++ src/Pages/Dashboard/CardBox.jsx
@@ -1,81 +1,85 @@
 import React, { useState } from "react";
+import EditBudget from "./EditBudget";
+import { makeAuthenticatedApiCall } from "../Api.js";
 
-const CardBox = ({ items }) => {
-  const [view, setView] = useState(true);
+const CardBox = ({ item, setBudgets }) => {
+  const [edit, setEdit] = useState(true);
 
-  const handleView = () => {
-    setView(!view);
+  const handleEdit = () => {
+    setEdit(!edit);
+  };
+  const deleteBudget = (id) => {
+    let responses = makeAuthenticatedApiCall("delete", `budgets/${id}`);
+    responses.then((response) => {
+      setBudgets(response.data.payload.budgets);
+    });
   };
 
   return (
-    <div className="box">
-      <div className={`${view ? "card-box" : "list-box"}`}>
-        <div className="view">
-          {view ? (
-            <i className="fas fa-list" onClick={handleView}>
-              List view
-            </i>
-          ) : (
-            <i className="fas fa-square-full" onClick={handleView}>
-              Card view
-            </i>
-          )}
-        </div>
-        <div className="title">
-          <div className="w-30">Name</div>
-          <div className="w-30">Income</div>
-          <div className="w-30">Actual cost</div>{" "}
-          <div className="w-30">Budget</div>
-          <div className="w-30">Items</div>
-          <div className="w-30">Created</div>
-        </div>
-        {items.map((item) => {
-          return (
-            <div className="item" key={item.id}>
-              <i className="fas fa-edit"></i>
-              <div className="desc">
-                <div className="value b">{item.name}</div>
-              </div>
-              <div className="desc">
-                <div className="value">
-                  {" "}
-                  <span>Income - </span>
-                  {item.income}
-                </div>
-              </div>
-              <div className="desc">
-                <div className="value">
-                  {" "}
-                  <span>Actual cost - </span>
-                  {item.actualCost}
-                </div>
-              </div>
-              <div className="desc">
-                {" "}
-                <div className="value">
-                  {" "}
-                  <span>Budget - </span>
-                  {item.budgetedCost}
-                </div>
-              </div>
-              <div className="desc">
-                {" "}
-                <div className="value">
-                  <span>Items - </span>
-                  {item.itemCount}
-                </div>
-              </div>
-              <div className="desc">
-                {" "}
-                <div className="value">
-                  <span>Created - </span>
-                  {new Date(item.createdAt).toDateString()}
-                </div>
-              </div>
+    <div className="item" key={item.id}>
+      {edit ? (
+        <>
+          <i
+            className="fas fa-trash-alt trash"
+            onClick={() => deleteBudget(item.id)}
+          ></i>
+          <i className="fas fa-edit edit" onClick={handleEdit}></i>
+        </>
+      ) : (
+        <i className="fas fa-times close" onClick={handleEdit}></i>
+      )}
+      {edit ? (
+        <>
+          {" "}
+          <div className="desc">
+            <div className="value b">{item.name}</div>
+          </div>
+          <div className="desc">
+            <div className="value">
+              {" "}
+              <span>Income - </span>
+              {item.income}
             </div>
-          );
-        })}
-      </div>
+          </div>
+          <div className="desc">
+            <div className="value">
+              {" "}
+              <span>Actual cost - </span>
+              {item.actualCost}
+            </div>
+          </div>
+          <div className="desc">
+            {" "}
+            <div className="value">
+              {" "}
+              <span>Budget - </span>
+              {item.budgetedCost}
+            </div>
+          </div>
+          <div className="desc">
+            {" "}
+            <div className="value">
+              <span>Items - </span>
+              {item.itemCount}
+            </div>
+          </div>
+          <div className="desc">
+            {" "}
+            <div className="value">
+              <span>Created - </span>
+              {new Date(item.createdAt).toDateString()}
+            </div>
+          </div>{" "}
+        </>
+      ) : (
+        <EditBudget
+          name={item.name}
+          income={item.income}
+          id={item.id}
+          handleEdit={handleEdit}
+          setBudgets={setBudgets}
+        />
+      )}
     </div>
   );
 };
---
+++ src/Pages/Dashboard/EditBudget.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import { makeUpdateApiCall, makeAuthenticatedApiCall } from "../Api.js";
+
+const EditBudget = (props) => {
+  const [loading, setLoading] = useState(false);
+
+  const { setBudgets } = props;
+
+  const formik = useFormik({
+    initialValues: {
+      name: props.name,
+      income: props.income,
+    },
+
+    onSubmit: (values) => {
+      const { name, income } = values;
+      setLoading(true);
+      const responses = makeAuthenticatedApiCall("put", `budgets/${props.id}`, {
+        name,
+        income,
+      });
+      responses
+        .then((response) => {
+          setBudgets(response.data.payload.budgets);
+          setLoading(false);
+          props.handleEdit();
+        })
+        .catch((error) => {
+          console.log({ error });
+        });
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div className="w-100">
+        <label className={`${formik.values.name ? "shrink" : ""}`}>Name</label>
+        <input type="text" name="name" {...formik.getFieldProps("name")} />
+      </div>
+      <div className="w-100">
+        <label className={`${formik.values.income ? "shrink" : ""}`}>
+          Income
+        </label>
+        <input
+          type="number"
+          name="income"
+          {...formik.getFieldProps("income")}
+        />
+      </div>
+      <div className="w-100">
+        <button type="submit" disabled={loading} className="btn1">
+          Edit budget {loading && <i className="fas fa-spinner"></i>}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditBudget;
